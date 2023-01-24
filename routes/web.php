56da48d3c26904cccacbf372bb8debcb83313ded
AndreaDeCeglia <deceglia89@gmail.com>
2023-01-24 15:05:18 +0100

    Laravel and Vue linked

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->prefix('admin') //localhost:8080/admin/posts
+    ->name('admin.')
+    ->group(function(){
+        Route::get('/', 'HomeController@index')->name('index');
+        //controllers per crud
+        Route::resource('/posts', PostController::class);
 });
+
+Route::get('{any?}', function(){
+    return view('guest.home');
+})->where("any", ".*");
